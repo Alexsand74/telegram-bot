1e5cdcbe4cc3fd83c23370b0beacb7f12561942d
Alexsand74 <1974alexsand@gmail.com>
2023-12-07 22:28:31 +0300

    Telegram bot is written and working!

---
+++ src/main/java/pro/sky/telegrambot/model/NotificationTask.java
@@ -0,0 +1,49 @@
+package pro.sky.telegrambot.model;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class NotificationTask {
+
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    private long userId;
+    private String message;
+    private LocalDateTime notificationDateTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LocalDateTime getNotificationDateTime() {
+        return notificationDateTime;
+    }
+
+    public void setNotificationDateTime(LocalDateTime notificationDateTime) {
+        this.notificationDateTime = notificationDateTime;
+    }
+}
